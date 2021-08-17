37e08801e0a64f1a3d25ce4598f30e435514269b
Urvashi2007 <81462840+Urvashi2007@users.noreply.github.com>
2021-08-17 18:23:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function spanCode(){
+    window.alert("this is a span element")
+    document.getElementById("audio1").pause()
+
+}
+function divCode(){
+    window.alert("this is a div element")
+    document.getElementById("audio1").play()
+}
